5cf044129a88d61eea42c624bf1b7e750cf99811
DS Rathore <dsrathorebsw1234@gmail.com>
2022-01-21 14:36:21 +0530

    Mini Project

--- hooks-concept/src/Components/UseStateArray.js
+++ hooks-concept/src/Components/UseStateArray.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Components/UseStateArray.css';
 
 const UseStateArray = () => {
+    const bioData = [
+        {
+            id: 1, Name: "King", Age: 19
+        },
+        {
+            id: 2, Name: "DS", Age: 22
+        },
+        {
+            id: 3, Name: "Gaurang", Age: 36
+        },
+        {
+            id: 4, Name: "Ravi", Age: 14
+        }
+    ];
+    const [mydata, setmydata] = useState(bioData);
+    const clearData = () => {
+        setmydata([]);
+    }
     return (
         <>
             <div className="container">
-                <h1 className='one heading'>Name: Digpal & Age: 19</h1>
-                <button className='btn'>Clear</button>
+                {mydata.map((curElem) => {
+                    return (<h1 className='one heading' id={curElem.id}>Name: {curElem.Name} & Age: {curElem.Age}</h1>);
+                })
+                }
+                <button className='btn' onClick={clearData}>Clear</button>
             </div>
         </>
     );
