3843e1a0d1b71e03f3751595e8ac943e0402e383
DS Rathore <dsrathorebsw1234@gmail.com>
2022-01-23 12:58:55 +0530

    Add Login Form

--- hooks-concept/src/index.js
+++ hooks-concept/src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+
 // import App from './Components/App';
-import UseStateArray from "./Components/UseStateArray";
+// import UseStateArray from "./Components/UseStateArray";
+// import ShortCircuitEvaluation from "./Components/ShortcircuitEvaluation";
+import LoginForm from "./Components/Forms/LoginForm";
+
 
 ReactDOM.render(
     // <App />,
-    <UseStateArray />,
+    // <UseStateArray />,
+    // <ShortCircuitEvaluation/>,
+    <LoginForm />, 
     document.getElementById("root")
 );
