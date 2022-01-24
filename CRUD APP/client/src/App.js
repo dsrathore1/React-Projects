3dc8a6477b6220c29a977860180e374413d39960
DS Rathore <dsrathorebsw1234@gmail.com>
2022-01-24 22:24:41 +0530

    Add get query

--- CRUD APP/client/src/App.js
+++ CRUD APP/client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
@@ -9,19 +9,39 @@ function App() {
   const [crudEmail, setCRUDEmail] = useState('');
   const [crudBlog, setCRUDBlog] = useState('');
 
+  const [crudList, setCURDList] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/read").then((res) => {
+      setCURDList([res.data]);
+      console.log(res);
+    });
+  }, []);
+
   const addData = () => {
     axios.post("http://localhost:4000/insert", {
       crudName: crudName,
       crudAge: crudAge,
       crudEmail: crudEmail,
       crudBlog: crudBlog
+    }).then(() => {
+      setCURDList([
+        ...crudList,
+        {
+          crudName: crudName,
+          crudAge: crudAge,
+          crudEmail: crudEmail,
+          crudBlog: crudBlog
+        },
+      ]);
     });
   };
+
 
   return (
     <div className="App">
       <div className="container">
-        <h1 className="heading">CRUD Operation</h1>
+        <h1 className="heading" key={Date()}>CRUD Operation</h1>
         <label className="name">Name</label>
         <input type="text" placeholder="Enter your name" className="nameI" onChange={(event) => {
           setCRUDName(event.target.value);
@@ -38,7 +58,22 @@ function App() {
         <textarea placeholder="Enter your blog" className="blogI" onChange={(event) => {
           setCRUDBlog(event.target.value)
         }} />
-        <button class="btn" onClick={addData}>Click Me</button>
+        <button className="btn" onClick={addData}>Click Me</button>
+      </div>
+      <h1 className='heading2'>CRUD LIST</h1>
+      <div className="crudList">
+        {
+          crudList.map((curElem, key) => {
+            return (
+              <div key={key} className="list">
+                <h1> {curElem.crudName} </h1>
+                <h2> {curElem.crudAge} </h2>
+                <h2> {curElem.crudEmail} </h2>
+                <h2> {curElem.crudBlog} </h2>
+              </div>
+            );
+          })
+        }
       </div>
     </div>
   );
